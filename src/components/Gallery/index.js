1d17fcc16ee851016ecfdefbae97711498eaec64
Brittany Ganser <15brganser@gmail.com>
2022-06-18 23:34:28 -0600

    added dynamically rendered photos using hooks

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,28 +1,15 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React from "react";
-
 import { capitalizeFirstLetter } from "../../utils/helpers";
+import PhotoList from "../PhotoList";
 
-import photo from "../../assets/small/commercial/0.jpg";
+function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory;
 
-function Gallery(props) {
-    const currentCategory = {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    };
-    return (
-      <section>
-        <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-        <p>{currentCategory.name}</p>
-        <div className="flex-row">
-            <img
-              src={photo}
-              alt="Commercial Example"
-              className="img-thumbnail mx-1"
-            />
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList category={currentCategory.name}></PhotoList>
+    </section>
+  );
+}
 export default Gallery;
